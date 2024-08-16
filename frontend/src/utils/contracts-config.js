504376f9ef0d90f94bd175aa16c4e092a6159918
Ant <ant96325@gmail.com>
2024-08-16 09:46:53 +0900

    complete src

---
+++ frontend/src/utils/contracts-config.js
@@ -0,0 +1,7 @@
+
+export const stakingContractAddress = "0xeDe12C37543cC663B7DdF7c8273eaDC8096f23fe"
+export const nftContractAddress = "0xE7cdD73719442342701AA55E59F4661BC9707322"
+export const tokenContractAddress = "0xd8e48bcbecbD150db2CEb5a82104d4332dA91eF2"
+export const ownerAddress = "0x8FD144d5E4e8b7665990420ebA189724345061FA"
+export const networkDeployedTo = "17000"
+    
